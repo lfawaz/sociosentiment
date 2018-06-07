e6b2d8e1c80fdb6de33e2df9b412e6a487ae370e
Labib Fawaz <lfawaz@MacBook-Pro.local>
2018-06-06 23:19:44 -0400

    Add SVG Map

--- socio-frontend/src/components/governor.js
+++ socio-frontend/src/components/governor.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import Navbar from './navbar'
-
+import USAMap from './us_map'
 class Governor extends Component {
   render(){
     return (<div>
       <Navbar />
-      <div>US Map</div>
+      <div><USAMap /></div>
       <div><div>Democrat Selected Governor candidate List</div><div>Republican Selected Governor candidate List</div></div>
       </div>)
   }
--- socio-frontend/src/components/us_map.js
+++ socio-frontend/src/components/us_map.js
@@ -1,46 +1,11 @@
 import React, { Component } from 'react'
-import ReactUSA from 'react-usa'
-
+import { SVGMap, USA } from 'react-svg-map'
+import 'react-svg-map/lib/index.css'
+import './us_map.css'
 class USAMap extends Component {
   render() {
-    const mapboxAccessToken = "" // Your access token
-    const mapboxType = "streets";
-    const position = [37.0902, -95.7129];
-    const zoom = 3;
-    const data = [
-      {
-        name: "Nebraska",
-        values: [{label: "Capital", val: "Lincoln"}, {label: "Electoral Votes", val: 3}],
-        color: "#E31A1C"
-      }
-    ]
-    const stateStyle = { weight: 1, opacity: 1, color: '#666', dashArray: '3', fillOpacity: 0.7 };
-    const stateHoverStyle = { weight: 5, color: '#FFF', dashArray: '1', fillOpacity: 0.7 };
-    const excludeStates = ["District of Columbia", "Puerto Rico"];
 
-    return (
-      <div>
-
-        <ReactUSA
-          mapboxAccessToken={mapboxAccessToken} // Required
-          mapHeight="500px" // Required
-          mapWidth="100%"
-          className="container"
-          mapboxType={mapboxType}
-          mapCenter={position}
-          mapZoom={zoom}
-          mapScrollZoom={false}
-          neighborhoodOn={true}
-          tooltip={true}
-          tooltipSticky={false}
-          data={data}
-          stateStyle={stateStyle}
-          stateHoverStyle={stateHoverStyle}
-          excludeStates={excludeStates}
-        />
-
-      </div>
-    )
+    return (<div><SVGMap map={USA} /></div>)
   }
 }
 
